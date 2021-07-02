ec6be2949e8662e5a106e0641da2074593f84278
tongchuan <tongchuanxing@163.com>
2021-07-02 16:02:52 +0800

    udpte

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-const abd: number = 222
+
+const abd: number = 222;
 function App() {
   return (
     <div className="App">
