095954686dd71ca2757abae9b029d0d615446742
zhangtch <zhangtch@yonyou.com>
2021-07-16 17:07:43 +0800

    update

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,19 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import React, { useState, useEffect } from 'react';
 // import Navigation from '../components/Navigation';
+const element = document.createElement('div');
 
-function Test() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    setCount((prevCount) => (prevCount + 1));
-  }, []);
+interface childInterface {
+  width: number;
+}
 
-  
+interface stateInterface {
+  isShow: boolean;
+  child: childInterface;
+}
+
+function TestChild(props: childInterface) {
+  const { child } = props;
+  console.log(child, child.width);
   return (
-    <div>
-      Product
-      {count}
+    <div className="test-child">
+      <h1>Test</h1>
+      <p>Child</p>
     </div>
   );
 }
 
+class Test extends React.Component<any, stateInterface> {
+  constructor(props: any) {
+    super(props);
+    this.onFocus = this.onFocus.bind(this);
+    this.state = {
+      isShow: false,
+      child: {
+        width: 0
+      }
+    };
+  }
+
+  componentDidMount() {
+    element.setAttribute('id', 'test');
+    document.body.appendChild(element);
+  }
+
+  onFocus() {
+    this.setState({
+      isShow: true
+    });
+  }
+
+  render() {
+    const { isShow, child } = this.state;
+    return (
+      <div>
+        <input type="text" onFocus={this.onFocus} />
+        {isShow && ReactDOM.createPortal(<TestChild child={child} />, element)}
+      </div>
+    );
+  }
+}
+
 export default Test;
+// function Test() {
+//   const [count, setCount] = useState(0);
+//   useEffect(() => {
+//     setCount((prevCount) => (prevCount + 1));
+//   }, []);
+
+//   return (
+//     <div>
+//       Product
+//       {count}
+//     </div>
+//   );
+// }
+
+// export default Test;
